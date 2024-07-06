e08baf4b4759b37bcb49f8905c9171d761ea1c83
yuchienchen <yuchienc@yahoo.com>
2024-07-06 11:00:02 -0500

    Just print. No need to main()

--- password_maker.py
+++ password_maker.py
@@ -1,12 +1,5 @@
 def make_password(string):
-    password = string.replace('a', '@').replace('i', '!').replace('o', '0').replace('A', '@').replace('I', '!').replace('o', '0')
+    password = string.replace('a', '@').replace('i', '!').replace('o', '0').replace('A', '@').replace('I', '!').replace('O', '0')
     return password
 
-def main():
-    string = input("")
-    make_password(string)
-
-
-
-if __name__ == "__main__":
-    main()
+print(make_password("acbiouABCIOU"))
