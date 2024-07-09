24e7ed04b9f4e06240973acd955e49e64942605e
yuchienchen <yuchienc@yahoo.com>
2024-07-09 16:39:49 -0500

    update strings

---
+++ credit_card_total.py
@@ -0,0 +1,22 @@
+"""
+File: credit_card_total.py
+--------------------------
+This program totals up a credit card bill based on
+how much was spent at each store on the bill.
+"""
+
+
+INPUT_FILE = 'bill1.txt'
+
+
+def main():
+    """
+    Add your code (remember to delete the "pass" below)
+    """
+    pass
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
